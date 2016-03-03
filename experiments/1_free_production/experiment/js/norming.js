5674d283fea10af1b8d18fdaf5eaf81161eb08fa
thegricean <judithdegen@gmail.com>
2016-03-02 19:02:44 -0800

    updated pilot

--- experiments/1_free_production/experiment/js/norming.js
+++ experiments/1_free_production/experiment/js/norming.js
@@ -28,49 +28,144 @@ function rect(point, center) {
   return {x: x, y: y};
 }
 
-function getPointLocations(n) {
-  var initpointlocations = [];
-  switch (n) {
-    case 3:
-      initpointlocations = [{x:45,y:30},{x:75,y:60},{x:105,y:30}];
-      break;
-    // fix this one and test the one before
-    case 4:
-      initpointlocations = [{x:40,y:20},{x:55,y:65},{x:95,y:65},{x:110,y:20}];
-      break;
-    case 5:
-      initpointlocations = [{x:35,y:20},{x:55,y:70},{x:75,y:45},{x:95,y:70},{x:115,y:20}];
-      break;
-    case 6:
-      initpointlocations = [{x:35,y:20},{x:45,y:70},{x:75,y:45},{x:75,y:70},{x:115,y:20},{x:110,y:70}];     
-      break;
-    case 9:
-      initpointlocations = [{x:25,y:20},{x:45,y:35},{x:70,y:20},{x:95,y:35},{x:115,y:20},{x:30,y:55},{x:50,y:70},{x:75,y:55},{x:100,y:70}];     
-      break;      
-    case 10:
-      initpointlocations = [{x:25,y:20},{x:45,y:35},{x:70,y:20},{x:95,y:35},{x:115,y:20},{x:30,y:55},{x:50,y:70},{x:75,y:55},{x:100,y:70},{x:115,y:55}];      
-      break;
-    case 15:
-      initpointlocations = [{x:35,y:20},{x:35,y:70},{x:75,y:45},{x:75,y:70},{x:115,y:20},{x:115,y:70}];     
-      break;
 
+function get100Points(n_total,n_target,tcolor,ocolor,w,h,radius) {
+  var points = [];
+  var targetcolor = tcolor;
+  var othercolor = ocolor;
+  var pointcolors = _.shuffle(fillArray(targetcolor,n_target).concat(fillArray(othercolor,n_total-n_target)));
+
+  var pcnt = 0;
+  var y = 24.5;
+
+  for (var i = 0; i < 4; i ++) {
+    for (var x = 23; x < 600; x+=46) {
+      points.push({x:x+uniform(-13,13),y:y+uniform(-13,13),color:pointcolors[pcnt]});
+      pcnt++;
+    }
+    y = y + 49;
+    for (var x = 24.5; x < 600; x+=49) {
+      points.push({x:x+uniform(-13,13),y:y+uniform(-13,13),color:pointcolors[pcnt]});
+      pcnt++;
+    }
+    y = y + 49;
   }
-  return initpointlocations;
+
+  return points;
 }
 
-function getPoints(n_total,n_target,tcolor,ocolor) {
+function get5Points(n_total,n_target,tcolor,ocolor,w,h,radius) {
   var points = [];
-  var initpointlocations = getPointLocations(n_total);
+  var targetcolor = tcolor;
+  var othercolor = ocolor;
+  var pointcolors = _.shuffle(fillArray(targetcolor,n_target).concat(fillArray(othercolor,n_total-n_target)));
+
+  var pcnt = 0;
+
+  points.push({x:100+uniform(-80,80),y:100+uniform(-60,60),color:pointcolors[pcnt]});
+  pcnt++;
+  points.push({x:300+uniform(-80,80),y:100+uniform(-60,60),color:pointcolors[pcnt]});
+  pcnt++;
+  points.push({x:500+uniform(-80,80),y:100+uniform(-60,60),color:pointcolors[pcnt]});
+  pcnt++;
+  points.push({x:150+uniform(-100,100),y:300+uniform(-60,60),color:pointcolors[pcnt]});
+  pcnt++;
+  points.push({x:350+uniform(-100,100),y:300+uniform(-60,60),color:pointcolors[pcnt]});
+
+  return points;
+}
+
+function get10Points(n_total,n_target,tcolor,ocolor,w,h,radius) {
+  var points = [];
+  var targetcolor = tcolor;
+  var othercolor = ocolor;
+  var pointcolors = _.shuffle(fillArray(targetcolor,n_target).concat(fillArray(othercolor,n_total-n_target)));
+
+  var pcnt = 0;
+  var cnt = 0
+  for (var y = 67; y < 400; y += 133) {
+    if (cnt % 2 == 0) {
+      for (var x = 100; x < 600; x+=200) {
+        points.push({x:x+uniform(-80,80),y:y+uniform(-40,40),color:pointcolors[pcnt]});
+        pcnt++;
+      }
+      } else {
+        for (var x = 75; x < 600; x+=150) {
+          points.push({x:x+uniform(-60,60),y:y+uniform(-40,40),color:pointcolors[pcnt]});
+        pcnt++;
+      }
+    }
+    cnt++;
+  }
+  return points;
+}
+
+function get25Points(n_total,n_target,tcolor,ocolor,w,h,radius) {
+  var points = [];
+  var targetcolor = tcolor;
+  var othercolor = ocolor;
+  var pointcolors = _.shuffle(fillArray(targetcolor,n_target).concat(fillArray(othercolor,n_total-n_target)));
+
+  var pcnt = 0;
+
+  for (var y = 40; y < 400; y += 80) {
+    for (var x = 60; x < 600; x+=120) {
+      points.push({x:x+uniform(-30,30),y:y+uniform(-25,25),color:pointcolors[pcnt]});
+      pcnt++;
+    }
+  }
+  return points;
+}
+
+function getPoints(n_total,n_target,tcolor,ocolor,w,h,radius) {
+  console.log("width: "+w);
+  console.log("height: "+h);  
+  //var initpointlocations = getPointLocations(n_total),w,h;
   var targetcolor = tcolor;//acolors[cnt].color;
   var othercolor = ocolor;//bcolors[cnt].color;
   var pointcolors = _.shuffle(fillArray(targetcolor,n_target).concat(fillArray(othercolor,n_total-n_target)));
-  console.log(n_total,n_target)
-  console.log(pointcolors);
+  //console.log(n_total,n_target)
+  //console.log(pointcolors);
   var points = [];
-  for (var i=0; i < n_total; i++) {
-    samp = sampleMarblePosition(initpointlocations[i],6);
-    points[i] = {x:samp.x,y:samp.y,color:pointcolors[i]};
+  var x = uniform(radius*2,w-radius*2);
+  var y = uniform(radius*2,h-radius*2);
+  points.push({x:x,y:y,color:pointcolors[0]});
+
+  for (var i=1; i < n_total; i++) {
+    console.log(i);
+    var goodpointfound = false;
+    while (!goodpointfound) {
+      console.log(points);
+    //samp = sampleMarblePosition(initpointlocations[i],6);
+      var x = uniform(radius*2,w-radius*2);
+      var y = uniform(radius*2,h-radius*2);
+       console.log("x: "+x);
+       console.log("y: "+y);
+
+      var cnt = 0;
+      for (var p=0; p < points.length; p++) {
+        console.log(Math.abs(points[p].x - x));
+        console.log(Math.abs(points[p].y - y));
+        // console.log(radius*4);
+        if (Math.abs(points[p].x - x) < radius*1.5 || Math.abs(points[p].y - y) < radius*1.5) {
+          break;
+          //console.log("increased cnt: "+cnt+" out of a total of "+points.length);
+        } else {
+          cnt++;
+        } 
+      }
+      if (cnt == points.length) {
+        console.log("found a good point");
+        goodpointfound = true;
+        points.push({x:x,y:y,color:pointcolors[i]});
+      } else {
+        console.log("start over");
+      }
+    }
   }
+  
+  console.log(points);
+  console.log(n_total);
   return points;
 }
 
@@ -78,22 +173,44 @@ function draw(id,n_total,n_target,tcolor,ocolor){
   var canvas = document.getElementById(id);
   if (canvas.getContext){
     var ctx = canvas.getContext("2d");
-    canvas.width = 200;
-    canvas.height = 150;     
+    canvas.width = 600;
+    canvas.height = 400;     
+    var radius = 0;
+
+    if (n_total < 25) {
+      radius = 150/n_total;
+    } else {
+      if (n_total == 25) {
+        radius = 10;
+      } else {
+      radius = 6;
+    }}
 
   //paint the rectangle
-    var x = canvas.width / 2;
-    var y = canvas.height / 4
+    // var x = canvas.width / 2;
+    // var y = canvas.height / 4
     var counterClockwise = true;
     ctx.rect(0,0,canvas.width,canvas.height);
     ctx.strokeStyle = 'black';
     ctx.stroke();
 
   //paint the marbles
-  points = getPoints(n_total,n_target,tcolor,ocolor); 
+  if (n_total == 5) {
+    points = get5Points(n_total,n_target,tcolor,ocolor,canvas.width,canvas.height,radius); 
+  } else {
+    if (n_total == 10) {
+      points = get10Points(n_total,n_target,tcolor,ocolor,canvas.width,canvas.height,radius);
+    } else {
+      if (n_total == 25) {
+        points = get25Points(n_total,n_target,tcolor,ocolor,canvas.width,canvas.height,radius);
+      } else {
+        points = get100Points(n_total,n_target,tcolor,ocolor,canvas.width,canvas.height,radius);
+      }
+    }
+  }
   for (var i=0; i < points.length; i++) {
     ctx.beginPath();
-    ctx.arc(points[i].x, points[i].y, 5, 0, 2*Math.PI, true);
+    ctx.arc(points[i].x, points[i].y, radius, 0, 2*Math.PI, true);
     ctx.fillStyle = points[i].color;
     ctx.closePath();
     ctx.fill();
@@ -137,9 +254,9 @@ function make_slides(f) {
 	    this.stim = stim;
 	    console.log(this.stim);      
 
-    	var contextsentence = "How would you describe this situation? Please provide at least two expressions (can be more than one word each) to fill in the blank.";
-      var blanksentence = "<strong>__________ dots are "+this.stim.color_target.colorword+".</strong>";
-    	$("#contextsentence").html(contextsentence);
+    	//var contextsentence = "How would you describe this situation? Please provide at least two expressions (can be more than one word each) to fill in the blank.";
+      var blanksentence = "How many dots are <strong>"+this.stim.color_target.colorword+"</strong>? <p> Please provide at least two expressions. An expression can be more than one word long.";
+    	//$("#contextsentence").html(contextsentence);
       $(".blanksentence").html(blanksentence);
       draw("situation",this.stim.n_total,this.stim.n_target,this.stim.color_target.color,this.stim.color_other.color);          
 	   },
@@ -162,9 +279,7 @@ function make_slides(f) {
     
     log_responses : function() {
         exp.data_trials.push({
-        "label" : this.stim.label,
         "slide_number_in_experiment" : exp.phase,
-        "item": this.stim.item,
         "rt" : Date.now() - _s.trial_start,
 	      "response" : this.stim.response,
 	      "color_target": this.stim.color_target.colorword,
@@ -181,11 +296,14 @@ function make_slides(f) {
       //if (e.preventDefault) e.preventDefault(); // I don't know what this means.
       exp.subj_data = {
         language : $("#language").val(),
+        languages : $("#languages").val(),
+        count : $("#count").val(),
         enjoyment : $("#enjoyment").val(),
         asses : $('input[name="assess"]:checked').val(),
         age : $("#age").val(),
         gender : $("#gender").val(),
         education : $("#education").val(),
+        colorblind : $("#colorblind").val(),
         comments : $("#comments").val(),
       };
       exp.go(); //use exp.go() if and only if there is no "present" data.
@@ -214,14 +332,8 @@ function make_slides(f) {
 function init() {
 
   function makeStim(i,n) {
-    var acolor = _.shuffle([{color:"#0066FF",colorword:"blue"},
-      {color:"#00CC00",colorword:"green"},{color:"#800080",colorword:"purple"},
-      {color:"#000000",colorword:"black"},{color:"#808080",colorword:"gray"}])[0];//
-    var bcolor = _.shuffle([{color:"#E60000",colorword:"red"},{color:"#FF70DB",colorword:"pink"},
-      {color:"#FF9147",colorword:"orange"},{color:"#FFFF00",colorword:"yellow"},
-      {color:"#8B4513",colorword:"brown"}])[0];
-    console.log(acolor);
-    console.log(bcolor);    
+    var acolor = _.shuffle([{color:"#0066FF",colorword:"blue"},{color:"#000000",colorword:"black"}])[0];//      {color:"#00CC00",colorword:"green"},{color:"#800080",colorword:"purple"},{color:"#808080",colorword:"gray"}])[0];//
+    var bcolor = _.shuffle([{color:"#E60000",colorword:"red"},{color:"#FF9147",colorword:"orange"}])[0];//,{color:"#FF70DB",colorword:"pink"},{color:"#FFFF00",colorword:"yellow"},{color:"#8B4513",colorword:"brown"}])[0];
 
     var shuffled = _.shuffle([acolor,bcolor]);
     color_target = shuffled[0];
@@ -255,7 +367,7 @@ function init() {
   }
 
   exp.all_stims = [];
-  var n_totals = [5, 10, 25, 100];
+  var n_totals = [5,10,25,100];
   for (var n = 0; n < n_totals.length; n++) {
     console.log(n_totals[n]);
     var intervals = getIntervals(n_totals[n]);
