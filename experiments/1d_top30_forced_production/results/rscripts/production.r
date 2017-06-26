24dac4e10c5b998669e4ea7ce92c919e53b1e2c7
JI Xiang <jimmyjx51@gmail.com>
2017-06-26 17:25:41 +0200

    Data for the 1d pilot

--- experiments/1d_top30_forced_production/results/rscripts/production.r
+++ experiments/1d_top30_forced_production/results/rscripts/production.r
@@ -1,9 +1,9 @@
 theme_set(theme_bw(18))
-setwd("~/Dropbox/Tuebingen17SS/RA/woq/experiments/1c/results")
-setwd("~/cogsci/projects/stanford/projects/woq/experiments/1c/results")
+setwd("~/Dropbox/Tuebingen17SS/RA/woq/experiments/1d_top30_forced_production/results")
+# setwd("~/cogsci/projects/stanford/projects/woq/experiments/1c/results")
 source("rscripts/helpers.r")
 
-d = read.table(file="data/production.csv",sep=",", header=T)
+d = read.table(file="data/1d-pilot.csv",sep=",", header=T)
 head(d)
 nrow(d)
 summary(d)
@@ -54,20 +54,74 @@ ggplot(d, aes(enjoyment)) +
   geom_histogram()
 
 d$response = tolower(d$response)
+
+# d$response_split = strsplit(as.character(d$response))
+
+## jx: The current series of substitutions here really look like hacks... I shall try to find a way to clean up the code later.
 d$response1 = sapply(strsplit(as.character(d$response),", "), "[", 1)
 d$response1 = gsub("\\[u''","NA",as.character(d$response1))
 d$response1 = gsub("\\[u'","",as.character(d$response1))
-d$response1 = as.factor(as.character(gsub("'","",as.character(d$response1))))
+
+# Somehow the responses with only one entry don't have quotes... Haven't figured out why. Just added these two additional substitutions. I don't think we have any word starting with u anyways.
+d$response1 = gsub("\\[u","",as.character(d$response1))
+d$response1 = gsub("*\\]","",as.character(d$response1))
+
+d$response1 = gsub("'","",as.character(d$response1))
+d$response1 = as.factor(as.character(gsub("\\]","",as.character(d$response1))))
 
 d$response2 = sapply(strsplit(as.character(d$response),", "), "[", 2)
-d$response2 = gsub("u''","NA",as.character(d$response2))
+d$response2 = gsub("u''\\]","NA",as.character(d$response2))
 d$response2 = gsub("u'","",as.character(d$response2))
-d$response2 = as.factor(as.character(gsub("'","",as.character(d$response2))))
+d$response2 = gsub("'","",as.character(d$response2))
+d$response2 = as.factor(as.character(gsub("\\]","",as.character(d$response2))))
 
 d$response3 = sapply(strsplit(as.character(d$response),", "), "[", 3)
 d$response3 = gsub("u''\\]","NA",as.character(d$response3))
 d$response3 = gsub("u'","",as.character(d$response3))
-d$response3 = as.factor(as.character(gsub("'\\]","",as.character(d$response3))))
+d$response3 = gsub("'","",as.character(d$response3))
+d$response3 = as.factor(as.character(gsub("\\]","",as.character(d$response3))))
+
+d$response4 = sapply(strsplit(as.character(d$response),", "), "[", 4)
+d$response4 = gsub("u''\\]","NA",as.character(d$response4))
+d$response4 = gsub("u'","",as.character(d$response4))
+d$response4 = gsub("'","",as.character(d$response4))
+d$response4 = as.factor(as.character(gsub("\\]","",as.character(d$response4))))
+
+d$response5 = sapply(strsplit(as.character(d$response),", "), "[", 5)
+d$response5 = gsub("u''\\]","NA",as.character(d$response5))
+d$response5 = gsub("u'","",as.character(d$response5))
+d$response5 = gsub("'","",as.character(d$response5))
+d$response5 = as.factor(as.character(gsub("\\]","",as.character(d$response5))))
+
+d$response6 = sapply(strsplit(as.character(d$response),", "), "[", 6)
+d$response6 = gsub("u''\\]","NA",as.character(d$response6))
+d$response6 = gsub("u'","",as.character(d$response6))
+d$response6 = gsub("'","",as.character(d$response6))
+d$response6 = as.factor(as.character(gsub("\\]","",as.character(d$response6))))
+
+d$response7 = sapply(strsplit(as.character(d$response),", "), "[", 7)
+d$response7 = gsub("u''\\]","NA",as.character(d$response7))
+d$response7 = gsub("u'","",as.character(d$response7))
+d$response7 = gsub("'","",as.character(d$response7))
+d$response7 = as.factor(as.character(gsub("\\]","",as.character(d$response7))))
+
+d$response8 = sapply(strsplit(as.character(d$response),", "), "[", 8)
+d$response8 = gsub("u''\\]","NA",as.character(d$response8))
+d$response8 = gsub("u'","",as.character(d$response8))
+d$response8 = gsub("'","",as.character(d$response8))
+d$response8 = as.factor(as.character(gsub("\\]","",as.character(d$response8))))
+
+d$response9 = sapply(strsplit(as.character(d$response),", "), "[", 9)
+d$response9 = gsub("u''\\]","NA",as.character(d$response9))
+d$response9 = gsub("u'","",as.character(d$response9))
+d$response9 = gsub("'","",as.character(d$response9))
+d$response9 = as.factor(as.character(gsub("\\]","",as.character(d$response9))))
+
+d$response10 = sapply(strsplit(as.character(d$response),", "), "[", 10)
+d$response10 = gsub("u''\\]","NA",as.character(d$response10))
+d$response10 = gsub("u'","",as.character(d$response10))
+d$response10 = gsub("'","",as.character(d$response10))
+d$response10 = as.factor(as.character(gsub("\\]","",as.character(d$response10))))
 
 d$proportion = d$n_target/d$n_total
 d$proportion_binned = cut(d$proportion,c(-.0001,.0001,.1,.2,.3,.4,.499,.501,.6,.7,.8,.9,.999,1))
@@ -88,8 +142,9 @@ ggplot(d, aes(x=proportion)) +
 ggsave("graphs/proportion_dist.pdf")
 
 gathered = d %>% 
-  select(response1,response2,response3,proportion_binned,n_total) %>%
-  gather(Order,Utterance,response1:response3,-proportion_binned,-n_total) 
+  # select(response1,response2,response3,proportion_binned,n_total) %>%
+  select(response1,response2,response3,response4,response5,response6,response7,response8,response9,response10,proportion_binned,n_total) %>%
+  gather(Order,Utterance,response1:response10,-proportion_binned,-n_total) 
 head(gathered)
 #gathered = as.factor(as.character(gathered))
 #gathered = gathered[order(gathered[,c("Count")],decreasing=T),]
@@ -135,8 +190,9 @@ table(droplevels(filter(d, n_total == 10, n_target == 3)$response1))
 
 ## JD : trying to get sense for the most frequently produced utterances (out of the 357 uniquely produced ones)
 gathered = d %>% 
-  select(response1,response2,response3,proportion,n_total) %>%
-  gather(Order,Utterance,response1:response3,-proportion,-n_total) 
+  # select(response1,response2,response3,proportion,n_total) %>%
+  select(response1,response2,response3,response4,response5,response6,response7,response8,response9,response10,proportion,n_total) %>%
+  gather(Order,Utterance,response1:response10,-proportion,-n_total) 
 gathered$Utterance = gsub("(^ +| +$)","",gathered$Utterance,perl=T)
 length(unique(gathered$Utterance))
 sorted = as.data.frame(sort(table(gathered$Utterance),decreasing=T))
